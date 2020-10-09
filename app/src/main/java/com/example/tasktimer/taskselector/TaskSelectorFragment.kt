1a24e051830ee6572d41b6a50701c22691bbee24
mbeebepdx <mbeebe08@gmail.com>
2020-10-09 12:32:18 -0700

    Implement navigation, get task selector fragment to display

--- app/src/main/java/com/example/tasktimer/taskselector/TaskSelectorFragment.kt
+++ app/src/main/java/com/example/tasktimer/taskselector/TaskSelectorFragment.kt
@@ -1,4 +1,25 @@
 package com.example.tasktimer.taskselector
 
-class TaskSelectorFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.tasktimer.R
+import com.example.tasktimer.databinding.FragmentTaskSelectorBinding
+
+class TaskSelectorFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentTaskSelectorBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_task_selector, container, false)
+
+        return binding.root
+    }
 }
